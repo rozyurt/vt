2d43e9235614343cfcbdace9db7efb421506806e
Rauf ÖZYURT <rozyurt@bil.omu.edu.tr>
2013-03-14 16:55:39 +0200

    Update departmentinsert.sql

--- departmentinsert.sql
+++ departmentinsert.sql
@@ -1,10 +1,10 @@
 insert into department (department_no, departmentname) 
 values 
     (1, 'bilgisayar mühendisliği'),
-    (2, 'makine mühendisliği')
-    (3, 'endüstri mühendisliği')
+    (2, 'makine mühendisliği'),
+    (3, 'endüstri mühendisliği'),
     (4, 'gıda mühendisliği'),
     (5, 'malzeme mühendisliği'),
     (6, 'çevre mühendisliği'),
-    (7, 'harita mühendisliği')
-    (8, 'elektrik elektronik mühendisliği'),
+    (7, 'harita mühendisliği'),
+    (8, 'elektrik elektronik mühendisliği');
