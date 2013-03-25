381c087dbc0f281dbb9c188ab9a8641d8ec23e9b
Rauf ÖZYURT <rozyurt@bil.omu.edu.tr>
2013-03-25 17:08:13 +0200

    Update publisher_insert.sql

--- publisher_insert.sql
+++ publisher_insert.sql
@@ -6,4 +6,3 @@ values
     (4, 'yıldız yayıncılık');
 
 
-select name, description from sales_order inner join sales_order_details on sales_order.s_order_no = sales_order_details.s_order_no inner join product_master on product_master.product_no = sales_order_details.product_no inner join client_master on client_master.client_no = sales_order.client_no where 10 < ( dely_date - s_order_date)
